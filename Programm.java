ded1f5b0c7698ce83542c6b817c0e205ddda3cea
ZhamsoUlzutuev <zhamsoulzutuev1234@gmail.com>
2026-02-09 21:39:21 +0800

    Update Programm.java

--- Programm.java
+++ Programm.java
@@ -1,31 +1,60 @@
+import java.time.ZonedDateTime;
 import java.util.*;
 
 public class Programm {
     //задание 1
     public static void main (String string[]){
         Scanner in = new Scanner(System.in);
+        System.out.println("ЗАДАНИЕ 1");
         System.out.println("Введите ИМЯ епт!");
         String name = in.nextLine();
         System.out.println("ПРИВЕТ " + name + " епт");
 
 // задание 2 Отобразить в окне консоли аргументы командной строки в обратном порядке.
+        System.out.println("ЗАДАНИЕ 2");
         for (int i = string.length - 1; i >= 0; i--){
             System.out.println(string[i]);
         }
 
 //задание 3
+        System.out.println("ЗАДАНИЕ 3");
         System.out.println("Введите число епт");
         int r = in.nextInt();
         ran(r);
 
+        System.out.println("ЗАДАНИЕ 4");
         System.out.println("Введи пароль епт");
         String ps = in.next();
         pswd(ps);
 
+        System.out.println("ЗАДАНИЕ 5");
+        sumANDmulti();
         //con();
+
+        System.out.println("ЗАДАНИЕ 6");
+        info(name);
     }
 
+    public static void info(String name){
+        System.out.println("ТИпо разраб = " + name);
+        System.out.println("Дата получения задания: 27.01.2026");
+        System.out.println(ZonedDateTime.now());
+    }
 
+    public static void sumANDmulti(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число епт");
+        int x = in.nextInt();
+        int s = 0;
+        int m = 1;
+        for(int i = 0; i < x; i++){
+            int a = in.nextInt();
+            s += a;
+            m *= a;
+        }
+        System.out.println("Сумма: " + s + " Произведение: " + m);
+
+    }
 
     public static void pswd(String ff){
 
@@ -49,7 +78,7 @@ public class Programm {
         }
     }
 
-// Вариант Б задание 8 Числа в порядке убывания частоты встречаемости чисел. функция con
+    // Вариант Б задание 8 Числа в порядке убывания частоты встречаемости чисел. функция con
     public static void con(){
         HashMap<Integer, Integer> map = new HashMap<>();
         Scanner in = new Scanner(System.in);
