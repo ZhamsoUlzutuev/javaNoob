b175b5512834bf961fc7cf2e91a2a16cc9fa8fb6
ZhamsoUlzutuev <zhamsoulzutuev1234@gmail.com>
2026-02-20 13:06:34 +0800

    Update program2.java

--- program2.java
+++ program2.java
@@ -1,37 +1,54 @@
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class program2 {
+    Scanner in = new Scanner(System.in);
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<Integer>();//для длин
-        ArrayList<String> list1 = new ArrayList<String>();//для значений
+        ArrayList<String> list1 = new ArrayList<String>();//для длин
         int n = in.nextInt();
-
+        String[] arr = new String[n];
 
         for(int i = 0; i < n; i++){
             int ch = in.nextInt();
-            list1.add(String.valueOf(ch));
+            arr[i] = String.valueOf(ch);
             list.add(String.valueOf(ch).length());
+            list1.add(String.valueOf(ch));
         }
-
         //mm(list);
-        bub(list1);
+        //bub(arr);
+        middle(list1);
     }
 
-    public static void bub(ArrayList<String> list){
+    public static void middle(ArrayList<String> list){
+        int cout = 0;
         for(int i = 0; i < list.size(); i++){
-            for(int j = 1; j < list.size() - i; j++){
-                if(list.get(j-1).length() > list.get(j).length());
-                String temp = list.get(j-1);
-                list.get(j-1) = list.get(j);
-                list.get(j) = temp;
+            cout += list.get(i).length();
+        }
+        int mid = cout / list.size();
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).length() > mid){
+                System.out.println(list.get(i) + " len: " + list.get(i).length() );
             }
+            else System.out.println(list.get(i) + " len: " + list.get(i).length() );
         }
     }
 
+///2
+    public static void bub(String[] arr){
+        for(int i = 0; i < arr.length ; i++){
+            for (int j = 0; j < arr.length - i - 1; j++){
+                if (arr[i].length() > arr[i+1].length()) {
+                    String temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+            System.out.println(arr[i]);
+        }
+    }
+///1
     public static void mm(ArrayList<Integer> list){
         int max = 1;
         int min = 1;
